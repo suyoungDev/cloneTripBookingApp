40073fd034984ee5987c8ab9ad88527d9f0d8932
mochapoke <71932072+mochapoke@users.noreply.github.com>
2020-11-20 12:07:02 +0900

    추가: 화면 구성

---
+++ screens/Onboarding.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import {
+    StyleSheet,
+    SafeAreaView,
+    View,
+    Text,
+    Image,
+    TouchableOpacity
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import { images, COLORS, FONTS, SIZES } from '../constants';
+import styled from 'styled-components';
+
+const Container = styled.SafeAreaView`
+  flex:1;
+  background-color: ${COLORS.white};
+`;
+const ImageView = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+const ClickButton = styled.TouchableOpacity`
+  margin-top: ${SIZES.padding * 2}px;
+  width: 70%;
+  height: 50px;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Onboarding = () =>{
+  return (
+    <Container>
+      <ImageView>
+        <Image
+          source={images.onboardingImage}
+          resizeMode="contain"
+          style={{
+            width: '100%',
+            height: '100%'
+          }} />
+      </ImageView>
+
+      <ImageView>
+        <View style={{alignItems: 'center', marginHorizontal: SIZES.padding}}>
+          <Text style={{...FONTS.h2}}>Digital Ticket</Text>
+          <Text style={{color: COLORS.gray, marginTop: SIZES.padding, textAlign: 'center', ...FONTS.body3}}>Easiest way to buy tickets for your travel, business trips, transportation, lodging and culication.</Text>
+        </View>
+
+      <ClickButton>
+        <TouchableOpacity 
+          style={{
+            backgroundColor: '#46aeff', 
+            height: '100%', width: '100%', 
+            elevation: 1, borderRadius: 15}}
+        >
+          <LinearGradient
+            style={{ 
+              height: '100%', width:'100%', 
+              alignItems:'center', justifyContent:'center', 
+              borderRadius: 15 }}
+            colors={['#46aeff', '#5884ff']}
+            satrt={{x:0, y:0}}
+            end={{x:1, y:1}}
+          >
+          <Text style={{color: COLORS.white, ...FONTS.h3, letterSpacing: 2}}>start!</Text>
+          </LinearGradient>
+        </TouchableOpacity>
+      </ClickButton>
+      </ImageView>
+    </Container>
+  )
+};
+
+const styles = StyleSheet.create({
+  shadow:{
+    elevation: 10,
+  }
+})
+
+export default Onboarding;
