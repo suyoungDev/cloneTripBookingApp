02cce1560a72bfbae930a1c595c4451d3bf0b36f
mochapoke <71932072+mochapoke@users.noreply.github.com>
2020-11-20 19:39:04 +0900

    추가

--- screens/DestinationDetail.js
+++ screens/DestinationDetail.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import styled from 'styled-components';
+import {COLORS, FONTS, icons, images, SIZES} from '../constants/';
+
+const Container = styled.View`
+  flex: 1;
+  background-color: COLORS.white;
+`;
 
 const DestinationDetail = () =>{
   return (
